e787440ff4b817747cb9a72788c130be99d0156f
Hafizhkhairullah <145668572+Hafizhkhairullah@users.noreply.github.com>
2024-07-18 11:06:15 +0700

    Update UAS_Kriptografi.py

--- UAS_Kriptografi.py
+++ UAS_Kriptografi.py
@@ -1,16 +1,12 @@
-# caesar_cipher.py
 def encrypt(text, shift):
     result = ""
 
-    # transverse the plain text
     for i in range(len(text)):
         char = text[i]
 
-        # Encrypt uppercase characters
         if char.isupper():
             result += chr((ord(char) + shift - 65) % 26 + 65)
 
-        # Encrypt lowercase characters
         else:
             result += chr((ord(char) + shift - 97) % 26 + 97)
 
@@ -20,8 +16,6 @@ def encrypt(text, shift):
 def decrypt(text, shift):
     return encrypt(text, -shift)
 
-
-# Driver code
 if __name__ == "__main__":
     text = input("Enter the text to encrypt: ")
     shift = int(input("Enter the shift number: "))
